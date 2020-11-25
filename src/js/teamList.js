ebf7f776ff3407427d659831959a3721c2c548cf
Anastasiia Yermakova <yermak1037@gmail.com>
2020-11-25 13:58:11 +0200

    fix team photo

--- src/js/teamList.js
+++ src/js/teamList.js
@@ -1,50 +1,50 @@
 export default [
-	{
-		name: "Roman Voznyy",
-		photo: "./images/team/roman.jpeg",
-		role: "Team Leader",
-		git: "https://github.com/RomanVoznyy"
-	},
-	{
-		name: "Anastasiia Yermakova",
-		photo: "./images/team/anastasiia.jpeg",
-		role: "Developer",
-		git: "https://github.com/yermakovaa"
-	},
-	{
-		name: "Elena Kharchenko",
-		photo: "./images/team/elena-kh.jpeg",
-		role: "Developer",
-		git: "https://github.com/olena-kharchenko"
-	},
-	{
-		name: "Vadym Melnik",
-		photo: "./images/team/vadym-m.jpeg",
-		role: "Developer",
-		git: "https://github.com/Powerman-code"
-	},
-	{
-		name: "Vadym Usov",
-		photo: "./images/team/vadym-u.jpeg",
-		role: "Developer",
-		git: "https://github.com/vertraut"
-	},
-	{
-		name: "Valentyn Yakubovskiy",
-		photo: "./images/team/valentin.jpeg",
-		role: "Developer",
-		git: "https://github.com/valentinyak"
-	},
-	{
-		name: "Yaroslav Petrovskiy",
-		photo: "./images/team/yaroslav.jpg",
-		role: "Developer",
-		git: "https://github.com/petrovskyi-Hub"
-	},
-	{
-		name: "Elena Panasyuk",
-		photo: "./images/team/elena-p.jpg",
-		role: "Scrum Master",
-		git: "https://github.com/elenapanasyuk"
-	},
-]
+  {
+    name: 'Roman Voznyy',
+    photo: 'https://ca.slack-edge.com/T017KJVP6TB-U017A1X8RK7-1b0be770772e-512',
+    role: 'Team Leader',
+    git: 'https://github.com/RomanVoznyy',
+  },
+  {
+    name: 'Anastasiia Yermakova',
+    photo: 'https://ca.slack-edge.com/T017KJVP6TB-U017LFQ1PCH-fa9a0639f357-512',
+    role: 'Developer',
+    git: 'https://github.com/yermakovaa',
+  },
+  {
+    name: 'Elena Kharchenko',
+    photo: 'https://ca.slack-edge.com/T017KJVP6TB-U0177QLEM5Y-2cdc1787f0c0-512',
+    role: 'Developer',
+    git: 'https://github.com/olena-kharchenko',
+  },
+  {
+    name: 'Vadym Melnik',
+    photo: 'https://ca.slack-edge.com/T017KJVP6TB-U0176GWNTK7-6049b80ddd52-512',
+    role: 'Developer',
+    git: 'https://github.com/Powerman-code',
+  },
+  {
+    name: 'Vadym Usov',
+    photo: 'https://ca.slack-edge.com/T017KJVP6TB-U017LEA78QZ-409715626b26-512',
+    role: 'Developer',
+    git: 'https://github.com/vertraut',
+  },
+  {
+    name: 'Valentyn Yakubovskiy',
+    photo: 'https://ca.slack-edge.com/T017KJVP6TB-U0171CCDXRC-eddb3d19b58a-512',
+    role: 'Developer',
+    git: 'https://github.com/valentinyak',
+  },
+  {
+    name: 'Yaroslav Petrovskiy',
+    photo: 'https://ca.slack-edge.com/T01F0NBB210-U01FJANFE76-0a41627dfb29-512',
+    role: 'Developer',
+    git: 'https://github.com/petrovskyi-Hub',
+  },
+  {
+    name: 'Elena Panasyuk',
+    photo: 'https://ca.slack-edge.com/T017KJVP6TB-U0177TASSAW-826ea43342d7-512',
+    role: 'Scrum Master',
+    git: 'https://github.com/elenapanasyuk',
+  },
+];
